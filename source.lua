a79b3c5fe6fa9d00dee098d94b4d5829f9e6aaef
Zed128 <118735074+Zed128@users.noreply.github.com>
2023-10-02 18:46:09 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -164,12 +164,13 @@ if get_hidden_gui or gethui then
 	local hiddenUI = get_hidden_gui or gethui
 	local Main = Instance.new("ScreenGui")
 	Main.Name = randomString()
+	syn.protect_gui(Main)
 	Main.Parent = COREGUI
 	PARENT = Main
 elseif (not is_sirhurt_closure) and (syn and syn.protect_gui) then
 	local Main = Instance.new("ScreenGui")
 	Main.Name = randomString()
-	--syn.protect_gui(Main)
+	syn.protect_gui(Main)
 	Main.Parent = COREGUI
 	PARENT = Main
 elseif COREGUI:FindFirstChild('RobloxGui') then
@@ -177,6 +178,7 @@ elseif COREGUI:FindFirstChild('RobloxGui') then
 else
 	local Main = Instance.new("ScreenGui")
 	Main.Name = randomString()
+	syn.protect_gui(Main)
 	Main.Parent = COREGUI
 	PARENT = Main
 end
